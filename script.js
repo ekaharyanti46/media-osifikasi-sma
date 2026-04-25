0b63c8a0f14076a587fbaea87fd82fa16da3ecc0
ekaharyanti46 <ummuyumnasyifa@gmail.com>
2026-04-25 20:55:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function cekJawaban(status) {
         "❌ Jawaban Salah! Skor Kamu: " + skor;
     }
 }
+var acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
