e7a1fcbc292701f27d4ad3c3a70c7d2a9550851d
ekaharyanti46 <ummuyumnasyifa@gmail.com>
2026-04-25 20:22:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function bukaMateri() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("quiz").style.display = "none";
-    document.getElementById("materi").style.display = "block";
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content');
+
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    document.getElementById(sectionId).classList.remove('hidden');
 }
 
-function bukaQuiz() {
-    document.getElementById("home").style.display = "none";
-    document.getElementById("materi").style.display = "none";
-    document.getElementById("quiz").style.display = "block";
+let skor = 0;
+
+function cekJawaban(status) {
+    if(status === 'Benar') {
+        skor = 100;
+        document.getElementById('hasil').innerHTML =
+        "🎉 Jawaban Benar! Skor Kamu: " + skor;
+    } else {
+        skor = 0;
+        document.getElementById('hasil').innerHTML =
+        "❌ Jawaban Salah! Skor Kamu: " + skor;
+    }
 }
